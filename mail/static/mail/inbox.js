69cf2fd7c321d0a7d2f0188b44fcddae4930acaf
unknown <robbygunawan94@gmail.com>
2022-04-16 09:10:08 +0200

    Complete tests.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -44,16 +44,45 @@ function send_email(event) {
 }
 
 
-function compose_email() {
+function compose_email(id) {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#the_email').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  if (id !== undefined) {
+    fetch(`/emails/${id}`)
+    .then(response => response.json())
+    .then(email => {
+      document.querySelector('#compose-recipients').value = `${email.sender}`;
+      if (email.subject.slice(0, 3) !== "Re:") {
+        document.querySelector('#compose-subject').value = `Re: ${email.subject}`;
+      } else {
+        document.querySelector('#compose-subject').value = `${email.subject}`;
+      }
+      document.querySelector('#compose-body').value = `On ${email.timestamp} ${email.sender} wrote : ${email.body}`;
+    })
+  }
+}
+
+function archive(id) {
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+    return fetch(`/emails/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        archived: !email.archived
+      })
+    })
+  })
+  .then(() => load_mailbox('inbox'));
 }
 
 function load_mailbox(mailbox) {
@@ -61,6 +90,7 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#the_email').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -72,19 +102,56 @@ function load_mailbox(mailbox) {
     if (emails.error) {
         alert(emails.error);
     }
-
     for (let each_email of emails) {
+      if ((each_email.archived === true) && (mailbox === 'inbox')) {
+        continue;
+      }
+
       let email_line = document.createElement('div');
-      email_line.textContent = `${each_email.sender}    ${each_email.subject}   ${each_email.timestamp}`;
-      email_line.style.backgroundColor = "gray";
-      email_line.style.border = "thin solid #000000"
+      email_line.setAttribute("class", "mail_line");
+      email_line.innerHTML = `<div>${each_email.sender}</div>    <div>${each_email.subject}</div>   <div>${each_email.timestamp}</div>`;
       if (each_email.read === false) {
         email_line.style.backgroundColor = "orange";
       }
+
+      // Event listener for viewing individual email
+      email_line.addEventListener('click', (event) => {
+        document.querySelector('#emails-view').style.display = 'none';
+        document.querySelector('#the_email').style.display = 'block';
+        
+        fetch(`/emails/${each_email.id}`)
+        .then(response => response.json())
+        .then(email => {
+          let an_email = document.querySelector("#the_email");
+          
+          an_email.innerHTML = `
+            <p>From : ${email.sender}</p>
+            <p>To : ${email.recipients}</p>
+            <p>Subject : ${email.subject}</p>
+            <p>Timestamp : ${email.timestamp}</p>
+            <button type="button" class="btn btn-outline-primary" id="reply">Reply</button>
+            <button type="button" class="btn btn-outline-primary" id="archive">Archive</button>
+            <hr>
+            <p>${email.body}</p>
+            `;
+            if (email.archived === true) {
+              document.querySelector('#archive').innerHTML = "Unarchive";
+            }
+            document.querySelector('#archive').onclick = (event) => archive(each_email.id);
+            document.querySelector('#reply').onclick = () => compose_email(each_email.id);
+        });
+
+        fetch(`/emails/${each_email.id}`, {
+          method: 'PUT',
+          body: JSON.stringify({
+              read: true
+          })
+        });
+        
+        
+      });
       document.querySelector('#emails-view').append(email_line);
     }
-
-
   });
-
 }
+
